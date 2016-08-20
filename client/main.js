47d33e88d412dcb8d79d724087f565846ee78240
Cyril Hong <cyrilhong@gmail.com>
2016-08-20 21:24:59 +1000

    api update

--- client/main.js
+++ client/main.js
@@ -7,10 +7,26 @@ Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 
+  loadData();
+  initSessionVars();
+
   // Meteor.call('getDataApi',function(res) {
   //
   //   console.log(res);
   // });
+
+  // this.matches = new ReactiveVar([]);
+  this.superValueGet = new ReactiveVar([]);
+
+  var self = this;
+  // Meteor.call('getMatches', function(error, result) {
+  //   if (result)
+  //     self.matches.set(result);
+  // });
+  Meteor.call('getDataApi', function(error, result) {
+    if (result)
+      self.superValueGet.set(result);
+  });
 
 
 });
@@ -18,24 +34,55 @@ Template.hello.onCreated(function helloOnCreated() {
 
 Template.hello.onRendered = function(){
 
-    Meteor.call('getDataApi',function(err, response) {
-      // Session.set('getDataApi', response);
-      console.log(response);
-  });
+  //   Meteor.call('getDataApi',function(err, response) {
+  //     // Session.set('getDataApi', response);
+  //     console.log(response);
+  // });
 
 };
 
 Template.hello.helpers({
   counter() {
     return Template.instance().counter.get();
-  }
+  },
+
+  // getDataApi: (res) => {
+  //   Meteor.call('getDataApi', function (error, result) {
+  //     Session.set('getData', result);
+  //     console.log(result);
+  //
+  //   });
+  //
+  // },
+  passData: function() {
+    console.log("get value = "+Template.instance().superValueGet.get());
+    return Template.instance().superValueGet.get();
+  },
+  // matches: function() {
+  //   console.log(Template.instance().matches.get());
+  //   return Template.instance().matches.get();
+  // }
 
 
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  }
+  // 'click button'(event, instance) {
+  //   // increment the counter when button is clicked
+  //   instance.counter.set(instance.counter.get() + 1);
+  // }
 });
+
+let initSessionVars = () => {
+  Session.set('getDataApi', []);
+};
+
+let loadData = () => {
+  // var self = this;
+  initSessionVars();
+  // Asynchronous call to get total number of records available at the endpoint:
+  Meteor.call('getDataApi', function (error, result) {
+    Session.set('getData', result);
+    console.log(result);
+  });
+};
--- server/main.js
+++ server/main.js
@@ -2,10 +2,30 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.methods({
     getDataApi: function () {
-        Meteor.http.call("GET",'http://rest.biolabeler.com/api/v1/type',function(error,result){
-            console.log(result);
-            return result;
-        });
+        var url = "http://rest.biolabeler.com/api/v1/type";
+        try {
+            var data = HTTP.get(url).data.data;
+            console.log("data = "+data);
+            return data;
+        } catch (e) {
+            return [];
+        }
+        // Meteor.http.call("GET",'http://rest.biolabeler.com/api/v1/type',function(){
+        //     var data = this.data;
+        //     console.log("this is data"+ data);
+        //     console.log("data length "+ data.length);
+        //     return data.data;
+        // });
+    },
+    getMatches: function() {
+        var url = "http://api.football-data.org/alpha/teams/73/fixtures";
+        try {
+            var fixtures = HTTP.get(url).data.fixtures;
+            console.log("fixtures = "+fixtures);
+            return fixtures;
+        } catch (e) {
+            return [];
+        }
     }
 });
 
@@ -19,6 +39,8 @@ Meteor.startup(() => {
     //     });
     // }
 });
+
+
 
 /**
  * Created by v222000 on 5/17/16.
